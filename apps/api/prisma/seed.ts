a66f960dc8169cb80a5cf97e2c7a876429c2c449
Donald Wu <yeukfei02@gmail.com>
2023-01-01 18:52:01 +0800

    feat: 🎸 add createIncidents in seed.ts

--- apps/api/prisma/seed.ts
+++ apps/api/prisma/seed.ts
@@ -1,11 +1,12 @@
-import { PrismaClient, UserRole } from '@prisma/client';
+import { PrismaClient, IncidentType, UserRole, Status } from '@prisma/client';
 import bcrypt from 'bcryptjs';
-// import { faker } from '@faker-js/faker';
+import { faker } from '@faker-js/faker';
 
 const prisma = new PrismaClient();
 
 (async () => {
   await createUsers();
+  await createIncidents();
 })();
 
 async function createUsers() {
@@ -26,5 +27,42 @@ async function createUsers() {
 
   await prisma.user.createMany({
     data: users,
+  });
+}
+
+async function createIncidents() {
+  const incidents: any[] = [];
+
+  const adminUser = await prisma.user.findFirst({
+    where: {
+      userRoles: {
+        hasEvery: [UserRole.ADMIN],
+      },
+    },
+  });
+  const normalUser = await prisma.user.findFirst({
+    where: {
+      userRoles: {
+        hasEvery: [UserRole.NORMAL_USER],
+      },
+    },
+  });
+
+  if (adminUser && normalUser) {
+    for (let i = 0; i < 10; i++) {
+      const data = {
+        title: faker.lorem.words(),
+        description: faker.lorem.text(),
+        type: i % 2 !== 0 ? IncidentType.HIGH : IncidentType.MEDIUM,
+        creator_id: adminUser.id,
+        assignee_id: normalUser.id,
+        status: Status.ASSIGNED,
+      };
+      incidents.push(data);
+    }
+  }
+
+  await prisma.incident.createMany({
+    data: incidents,
   });
 }
