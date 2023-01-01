2d59b7a6c7bc344c1e97bc7b6b976bbdbc36c6e7
Donald Wu <yeukfei02@gmail.com>
2023-01-01 21:41:13 +0800

    fix: 🐛 fix seed.ts

--- apps/api/prisma/seed.ts
+++ apps/api/prisma/seed.ts
@@ -1,6 +1,13 @@
-import { PrismaClient, IncidentType, UserRole, Status } from '@prisma/client';
+import {
+  PrismaClient,
+  Incident,
+  IncidentType,
+  UserRole,
+  Status,
+} from '@prisma/client';
 import bcrypt from 'bcryptjs';
 import { faker } from '@faker-js/faker';
+import { v4 as uuidv4 } from 'uuid';
 
 const prisma = new PrismaClient();
 
@@ -31,7 +38,7 @@ async function createUsers() {
 }
 
 async function createIncidents() {
-  const incidents: any[] = [];
+  const incidents: Incident[] = [];
 
   const adminUser = await prisma.user.findFirst({
     where: {
@@ -51,12 +58,15 @@ async function createIncidents() {
   if (adminUser && normalUser) {
     for (let i = 0; i < 10; i++) {
       const data = {
+        id: uuidv4(),
         title: faker.lorem.words(),
         description: faker.lorem.text(),
         type: i % 2 !== 0 ? IncidentType.HIGH : IncidentType.MEDIUM,
         creator_id: adminUser.id,
         assignee_id: normalUser.id,
         status: Status.ASSIGNED,
+        created_at: new Date(),
+        updated_at: new Date(),
       };
       incidents.push(data);
     }
